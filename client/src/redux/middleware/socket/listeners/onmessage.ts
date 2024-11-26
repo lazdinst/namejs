3c54a4c26af43c205eb78f9efa85bbe4d09df98c
lazdinst <lazdinst@gmail.com>
2024-11-26 09:29:13 -0500

    shared and fixed client build

--- client/src/redux/middleware/socket/listeners/onmessage.ts
+++ client/src/redux/middleware/socket/listeners/onmessage.ts
@@ -1,7 +1,5 @@
 import { AppDispatch } from "../../../store";
 import { WebsocketMessage } from "../types/websocket.types";
-import handleStatusMessageType from "./status";
-import handleIOMessageStatus from "./io";
 
 export const handleOnMessage = (event: MessageEvent, dispatch: AppDispatch) => {
   const message: WebsocketMessage = JSON.parse(event.data);
@@ -12,12 +10,6 @@ export const handleOnMessage = (event: MessageEvent, dispatch: AppDispatch) => {
   if (messageType === "gameState") {
     const platoons = message.payload.platoons;
     console.log("Platoons:", platoons);
-  }
-  if (messageType === "status") {
-    handleStatusMessageType(message, dispatch);
-  }
-
-  if (messageType === "io") {
-    handleIOMessageStatus(message, dispatch);
+    console.log(dispatch);
   }
 };
